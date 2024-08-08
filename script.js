7a065d7875a4ad1fbbde260832a95e4aa63f5bdb
prajwalgirhepunje <prajwalgirhepunje12@gmail.com>
2024-08-08 09:26:45 +0530

    previus and next button adeed

---
+++ script.js
@@ -0,0 +1,21 @@
+let Images = [ "img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg"];
+const MainElement =document.getElementById("main-element");
+let Imageindex = 0 ;
+ function previmage(){
+
+           if(Imageindex == 0){
+             Imageindex=Images.length-1;
+           }else{
+            Imageindex--;
+           }
+
+           MainElement.src=Images[Imageindex];
+ }
+ function nextimage(){
+    if(Imageindex == Images.length-1){
+        Imageindex = 0 ;
+    }else{
+        Imageindex++ ;
+    }
+    MainElement.src=Images[Imageindex];
+ }
